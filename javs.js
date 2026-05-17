22b434c61f6d7746bd435b1e7a7db4850c36789f
Basavaraj20 <153058248+Basavaraj20@users.noreply.github.com>
2026-05-17 13:51:37 +0530

    Add files via upload

---
+++ javs.js
@@ -0,0 +1,198 @@
+// 1) Create a variable using var and store your name.
+var name="Basavaraj";
+ //2) Print your name using console.log().
+console.log(name);
+
+//3)  Create a variable using let and store your age.
+let age =23
+
+//4) change the value of age and print it.
+age=24
+console.log(age);
+
+//5) create a constant variable for country name
+const country="india"
+
+//6) Print the country name.
+console.log(country);
+
+//7)  Try redeclaring var variable and observe output.
+var num = 18
+var num = 45
+console.log(num); //45
+
+//8) Try redeclaring let variable and observe error
+// let a=30
+// let a=18
+// console.log(a); //Uncaught SyntaxError: Identifier 'a' has already been declared
+
+//9) Try reassigning const variable and observe error.
+// const b=5
+// b=15
+// console.log(b); //javs.js:31 Uncaught TypeError: Assignment to constant variable.
+
+//10) Write difference between var and let.
+// var can be declared,reassigned and redeclared
+// il let can be only declared and reassigned cannot be redeclared
+
+//11) Write difference between let and const.
+// let can be declared and reassigned
+// const can be declared only
+
+// 12) predict output
+var a=10
+a=20
+var a = 30
+console.log(a);//30
+
+//13) predict output
+let b= 50
+b=60
+console.log(b); //60
+
+//14) predict output
+const c = 100
+console.log(c);
+
+//15) Use console.log() to print "Hello World".
+// console.log("Hello World");
+
+//16) Use alert() to show "Welcome User".
+// alert("Welcome User");
+
+//17)  Use confirm() to ask user:
+// "Do you like JavaScript?"
+// confirm("Do you like JavaScript")
+
+//18) use promt() to ask user name
+// let Username=  prompt ("enter your name");
+
+
+// 19)Print prompt value in console.
+// console.log(Username);
+
+//20) Use document.writeln() to print "Batch 33".
+document.writeln("Batch 33");
+
+//21) Create variable studentName and store your name.
+var studentName = "Basavaraj"
+
+// 22)Create variable studentAge and store your age.
+var studentAge = 24
+
+// 23) Print both variables together.
+console.log(studentName,studentAge);
+
+//24) Create a variable for mobile number.
+let mobileNumber = 6361403152;
+
+//25) Print mobile number using console.log().
+console.log(mobileNumber);
+
+//26) Create variable using var for favorite color.
+var color="black"
+
+//27) Reassign another color value.
+color="red"
+
+//28) create variables using let for marks
+let mark=25
+
+//29) Change marks value and print it.
+mark=35
+console.log(mark)
+
+//30) Create const variable for PI value.
+const PI = 3.14
+
+//31) print PI value
+console.log(PI);
+
+//32) predict output
+var x = 5
+var x=15
+console.log(x)//15
+
+//33) predict output
+let y = 25
+y = 35
+console.log(y)//35
+
+//34) find error 
+// const z = 40
+//  z=45
+// console.log(z)//Uncaught TypeError: Assignment to constant variable.
+
+//35) Create alert box with your college name.
+// alert("sharnbasav university");
+
+//36)  Ask user age using prompt().
+// let UserAge = prompt("Enter your age");
+
+//37) Print user age in console. 
+// console.log(UserAge);
+
+//38) Ask user confirmation:
+// "Did you complete HTML?"
+// let userCompletion = confirm("did you complete html")
+
+//39) Print confirm result in console.
+// console.log(userCompletion);
+
+//40) Print "JavaScript Class" using document.writeln().
+document.writeln("JavaScript Class");
+
+//41) create 3 variables
+// city,state,country
+let city = " kalaburgi";
+let state = "karnataka";
+let Country = "India";
+
+//42) Print all variables together.
+console.log(city,state,Country);
+
+//43) Create one example for var.
+var x1 = 15;
+x1=10;
+ var x1=5;
+console.log(x1);
+
+//44)  Create one example for let.
+let pet = "donkey"
+pet = "dog"
+console.log(pet);
+
+//45)Create one example for const.
+const BG = "B+ve"
+console.log(BG);
+
+//46) predict output
+var num = 100
+num = 200
+console.log(num);
+
+//47) predict output
+let price = 15
+price =10
+console.log(price);
+
+//48) predict output
+const password = 5555
+console.log(password);
+
+//49) Write rules of var.
+// var can be declared.
+// var can be reassigned.
+// var can be redeclared.
+
+
+//50) Write rules of let and const.
+// let can be declared.
+// let can be reassigned.
+// let cannot be redeclared.
+
+// const must be initialized while declaring.
+// const cannot be reassigned.
+// const cannot be redeclared.
+
+
